7c797ce89295e7e7b5d97a3e9b7ee65c65d23a13
rdua <raghav.dua@booking.com>
2023-06-04 12:21:24 +0200

    Add cleanup.sh

---
+++ run-db-in-k8s/cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl delete sts --all
+kubectl delete pods --all
+kubectl delete pv --all
+kubectl delete pvc --all
+kubectl delete svc --all
+kubectl delete cm --all
+
+echo "Done"
+
