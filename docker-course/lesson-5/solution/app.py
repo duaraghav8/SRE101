d4c68d62b524d5a6cd17df072338a93425bf15d3
duaraghav8 <duaraghav8@protonmail.com>
2023-11-10 14:08:05 +0100

    add note

--- docker-course/lesson-5/solution/app.py
+++ docker-course/lesson-5/solution/app.py
@@ -11,5 +11,7 @@ def hello():
     return "PONG"
 
 
+# The default port used by Flask to bind to is 5000.
+# So we need not explicitly specify unless we want to bind to some other port.
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
