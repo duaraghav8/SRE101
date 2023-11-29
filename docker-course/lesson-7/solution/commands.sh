f55c1e74f90720136c15a81fd17de86c83519f0e
duaraghav8 <duaraghav8@protonmail.com>
2023-11-29 14:52:14 +0100

    Lesson 7 assets

---
+++ docker-course/lesson-7/solution/commands.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# Build Image
+docker build -t voltest:latest .
+
+# Run container without volume mounting
+docker run -it --rm voltest
+
+# Create Volume
+docker volume ls
+docker volume create voltest
+docker volume ls
+
+# Run container with pre-created volume
+docker run -it --rm --volume 'voltest:/data' voltest
+
+# Volume operations
+docker inspect voltest
+docker rm voltest
+
+# Use --mount for more customization
+docker run \
+    -it \
+    --mount 'type=volume,src=voltest2,dst=/data,volume-driver=local' \
+    voltest
