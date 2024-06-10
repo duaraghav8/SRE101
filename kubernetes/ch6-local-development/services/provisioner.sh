7083443ccf9f612b80ed5b5def989023800d2fee
duaraghav8 <duaraghav8@protonmail.com>
2024-06-10 22:30:10 +0200

    add service a

---
+++ kubernetes/ch6-local-development/services/provisioner.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Create multi-node K8s cluster using Minikube
+# Submit k8s manifests of all services so they can start running in the k8s cluster
---
+++ kubernetes/ch6-local-development/services/service_a/app.py
@@ -0,0 +1,31 @@
+import requests
+from flask import Flask, jsonify
+import redis
+import logging
+
+app = Flask(__name__)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
+
+app.logger.setLevel(logging.INFO)
+
+
+@app.route('/complex_request', methods=['GET'])
+def talk_to_service_b():
+    app.logger.info("Sending request to Redis")
+    foo_value = redis_client.get("foo")
+
+    app.logger.info("Sending request to service B")
+    response = requests.get('http://service_b:5001/data')
+
+    return jsonify({
+        "redis_foo_response": foo_value,
+        "service_b_response": response.json()
+    })
+
+
+if __name__ == '__main__':
+    # Initialize redis with fake data, so it can
+    # be returned when the api controller asks for it
+    redis_client.set("foo", "bar")
+
+    app.run(host='0.0.0.0', port=5000)
