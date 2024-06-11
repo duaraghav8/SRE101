8a3f7157726b7201fe8b92491aafd2fd0063c1c5
duaraghav8 <duaraghav8@protonmail.com>
2024-06-11 16:17:09 +0200

    move db initialization logic to manifest

--- kubernetes/ch6-local-development/services/service_b/app.py
+++ kubernetes/ch6-local-development/services/service_b/app.py
@@ -16,24 +16,6 @@ def get_db_connection():
     return connection
 
 
-def populate_mysql_data():
-    conn = get_db_connection()
-
-    cursor = conn.cursor()
-
-    cursor.execute('DROP TABLE IF EXISTS test_table')
-    cursor.execute('CREATE TABLE test_table (name VARCHAR(255), title VARCHAR(255))')
-
-    query = "INSERT INTO test_table (name, title) VALUES (%s, %s)"
-    cursor.execute(query, ('raghav', 'sre'))
-    cursor.execute(query, ('alice', 'devops engineer'))
-    cursor.execute(query, ('bob', 'platform engineer'))
-    cursor.execute(query, ('deadpool', 'x man'))
-
-    conn.commit()
-    conn.close()
-
-
 @app.route('/data', methods=['GET'])
 def get_data():
     app.logger.info("Sending request for data to MySQL")
@@ -46,10 +28,6 @@ def get_data():
 
 
 if __name__ == '__main__':
-    # Populate MySQL with dummy data so that it is
-    # available when the api controller attempts to read it.
-    populate_mysql_data()
-
     app.run(host='0.0.0.0', port=5001)
 
 
