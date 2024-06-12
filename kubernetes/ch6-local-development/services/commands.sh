8322a91fc7ae4d63022a0b7c799c11a16213005b
duaraghav8 <duaraghav8@protonmail.com>
2024-06-12 17:51:15 +0200

    Add assets to run mirrord

--- kubernetes/ch6-local-development/services/commands.sh
+++ kubernetes/ch6-local-development/services/commands.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
-# Create a multi-node minikube cluster, ensure kubectl points to it
+# Commands to run everything as discussed in the video
+
+# Create a multi-node minikube cluster, ensure kubectl points to it.
+# This is just to test things locally.
+# If your cluster is running remotely, just ensure that
+# kubectl points to it.
+# TODO: rename k8s cluster to "staging" for more clarity
 minikube start --nodes 4 -p sre101
 
-# Run mysql, expose it to localhost and try connecting.
+# Run mysql, expose it to localhost and try connecting to & querying it.
 # It should already be initialized with the data.
 kubectl apply -f mysql/k8s-deployment.yaml
 minikube -p sre101 service mysql
 mysql -h localhost -P <MINIKUBE SERVICE PORT> --protocol=tcp -D sre101 -u root -ppassword
 
+# Run redis
 kubectl apply -f redis/k8s-deployment.yaml
 
+# Run Service B
 kubectl apply -f service_b/k8s-deployment.yaml
 
+# Run Service A
 kubectl apply -f service_a/k8s-deployment.yaml
+
+
+# After making some changes to Service A code locally,
+# Run mirrord to test it.
+# mirrord will run the service in the context of the
+# specified K8s Pod.
+# Before running, if required, you can create and start
+# a python virtualenv and install all dependencies of
+# service A.
+cd service_a
+mirrord exec --target pod/<POD NAME> python3 app.py
+
+# Now the app is mirrored. Run curl in a different terminal
+# to test the original functionality & the new changes.
+curl http://localhost:5000/complex_request
+curl http://localhost:5000/health
+
+# Steal all traffic on staging into local app
+# TODO
+
+# Run mirrord with specific configurtion
+mirrord exec -f mirrord-configs/steal-all-traffic.json
+mirrord exec -f mirrord-configs/steal-traffic-header-based.json
