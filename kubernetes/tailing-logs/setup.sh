002bbbf06d8f964f5b0197804042c3c264bc0d44
duaraghav8 <duaraghav8@protonmail.com>
2024-07-11 21:26:19 +0200

    update

--- kubernetes/tailing-logs/setup.sh
+++ kubernetes/tailing-logs/setup.sh
@@ -3,8 +3,11 @@
 # Start cluster
 minikube start -p sre101 --nodes 4
 
+kubectl create namespace production
+kubectl config set-context --current --namespace production
+
 # Deploy app
 kubectl apply -f ./deployment.yaml
 
 # Deploy daemonset
-kubectl apply -f https://k8s.io/examples/controllers/daemonset.yaml
+kubectl apply -f ./daemonset.yaml
