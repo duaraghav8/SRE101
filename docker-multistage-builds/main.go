4f777717760a1a14b35d05eaf689e4fa9bc1ce25
duaraghav8 <duaraghav8@protonmail.com>
2024-07-18 15:51:32 +0200

    improve multistage code

--- docker-multistage-builds/main.go
+++ docker-multistage-builds/main.go
@@ -13,7 +13,7 @@ func helloWorld(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
     http.HandleFunc("/", helloWorld)
-    fmt.Println("Hello server is now running")
+    fmt.Println("HTTP server is now running on port 8000")
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
