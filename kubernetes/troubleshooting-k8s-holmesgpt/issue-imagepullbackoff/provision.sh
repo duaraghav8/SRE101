99c263424ac1a6252a428ee628abea4ef6441943
duaraghav8 <duaraghav8@protonmail.com>
2024-08-22 19:56:19 +0200

    Add solution to authenticate with private registry

--- kubernetes/troubleshooting-k8s-holmesgpt/issue-imagepullbackoff/provision.sh
+++ kubernetes/troubleshooting-k8s-holmesgpt/issue-imagepullbackoff/provision.sh
@@ -13,3 +13,16 @@ kubectl get pods
 holmes ask --model "gpt-4o-2024-08-06" "Why is my payments pod not running? Explain the problem and suggest some solutions."
 
 holmes ask --model "gpt-4o-2024-08-06" "My payments-service Pod is not running because it is unable to pull the container image from my private docker registry. How can I authenticate with my dockerhub registry? Give me example code."
+
+
+###################
+# Solution
+###################
+
+source ~/dockerhub-access-token.sh
+kubectl create secret docker-registry dockerhub-access --docker-username duaraghav8 --docker-password=$DOCKERHUB_ACCESS_TOKEN
+
+# Then edit manifest to include the following under spec:
+
+#imagePullSecrets:
+#  - name: dockerhub-access
