54caa0ed145f4d8488182dc98eccac79181125e5
duaraghav8 <duaraghav8@protonmail.com>
2024-08-22 16:38:04 +0200

    add subsequent execution

--- kubernetes/troubleshooting-k8s-holmesgpt/issue-imagepullbackoff/provision.sh
+++ kubernetes/troubleshooting-k8s-holmesgpt/issue-imagepullbackoff/provision.sh
@@ -11,3 +11,5 @@ kubectl get pods
 
 
 holmes ask --model "gpt-4o-2024-08-06" "Why is my payments pod not running? Explain the problem and suggest some solutions."
+
+holmes ask --model "gpt-4o-2024-08-06" "My payments-service Pod is not running because it is unable to pull the container image from my private docker registry. How can I authenticate with my dockerhub registry? Give me example code."
