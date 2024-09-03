8966ce6d207bfe32d1a445c08422ff70e7f5c686
duaraghav8 <duaraghav8@protonmail.com>
2024-09-03 15:09:11 +0200

    install robusta

--- kubernetes/troubleshooting-k8s-holmesgpt/issue-prometheus-alert/provision.sh
+++ kubernetes/troubleshooting-k8s-holmesgpt/issue-prometheus-alert/provision.sh
@@ -3,6 +3,7 @@
 minikube start --nodes 4 -p staging
 
 # install prometheus
+# NOTE: You don't need this if you install robusta with built-in prometheus stack
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/kube-prometheus-stack
@@ -10,12 +11,14 @@ helm install prometheus prometheus-community/kube-prometheus-stack
 kubectl get pods -l "release=prometheus"
 
 # run in different tabs to be able to access them on your local (mac)
-minikube service prometheus-kube-prometheus-prometheus
-minikube service prometheus-kube-prometheus-alertmanager
+minikube -p staging service prometheus-kube-prometheus-prometheus
+minikube -p staging service prometheus-kube-prometheus-alertmanager
 
 # If minikube is deployed remotely and you're accessing prometheus UI from laptop
 kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-alertmanager 25000:8080
 kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-alertmanager 25001:9093
+
+# open <ip>:25001 in browser
 
 kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-prometheus 25002:8080
 kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-prometheus 25003:9090
@@ -24,6 +27,13 @@ kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-prometheus
 # Make sure to whitelist the port range in firewalls / security groups
 # After this, you can access the UIs at http://<public-ip>:2500{0,1,2,3}
 #
+
+# Install Robusta
+# Signup on saas and download the generated_value.yaml on server
+# Also add "enablePrometheusStack: true" in the yaml config file to use robusta in-built prom stack
+
+helm repo add robusta https://robusta-charts.storage.googleapis.com && helm repo update
+helm install robusta robusta/robusta -f ./generated_values.yaml --set clusterName=staging
 
 # deploy the cpu intensive application to K8s
 kubectl apply -f deployment.yaml
