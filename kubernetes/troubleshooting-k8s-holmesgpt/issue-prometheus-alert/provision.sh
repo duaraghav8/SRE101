49cd87bb3102d1ebbc63e5a96228df7c3afb55a5
duaraghav8 <duaraghav8@protonmail.com>
2024-09-06 10:18:08 +0200

    holmes integration

--- kubernetes/troubleshooting-k8s-holmesgpt/issue-prometheus-alert/provision.sh
+++ kubernetes/troubleshooting-k8s-holmesgpt/issue-prometheus-alert/provision.sh
@@ -31,7 +31,10 @@ kubectl port-forward --address 0.0.0.0 svc/prometheus-kube-prometheus-prometheus
 # (optional) Install Robusta
 # Signup on saas and download the generated_value.yaml on server
 # Also add "enablePrometheusStack: true" in the yaml config file to use robusta in-built prom stack
+# Also include the holmesgpt integration configuration in generated_values.yaml
+# https://docs.robusta.dev/master/configuration/ai-analysis.html
 
+kubectl create secret generic holmes-secrets --from-literal=openAiKey=XXXXXXXXXXXXXX
 helm repo add robusta https://robusta-charts.storage.googleapis.com && helm repo update
 helm install robusta robusta/robusta -f ./generated_values.yaml --set clusterName=staging
 
